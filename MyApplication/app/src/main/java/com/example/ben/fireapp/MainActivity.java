7c7ae452de486b2d21de8d6b2b89b85bf5003886
TheAppFoundry <wbh26@txstate.edu>
2017-02-03 17:13:00 -0600

    First commit Firebase practice App

---
+++ MyApplication/app/src/main/java/com/example/ben/fireapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ben.fireapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button submitData = (Button)findViewById(R.id.submitData);
+        final EditText textField = (EditText)findViewById(R.id.editText);
+
+        submitData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.setValue(textField.getText());
+            }
+        });
+
+    }
+}
